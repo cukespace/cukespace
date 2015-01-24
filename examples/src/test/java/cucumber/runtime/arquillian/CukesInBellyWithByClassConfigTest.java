d43825fc2eb91863081b7dc9002d715c91497c61
Jorge Gonzalez <jorgeejgonzalez@gmail.com>
2015-01-24 16:12:47 -0430

    removed useless import

--- examples/src/test/java/cucumber/runtime/arquillian/CukesInBellyWithByClassConfigTest.java
+++ examples/src/test/java/cucumber/runtime/arquillian/CukesInBellyWithByClassConfigTest.java
@@ -4,7 +4,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import cucumber.api.junit.Cucumber;
 import cucumber.runtime.arquillian.api.Features;
 import cucumber.runtime.arquillian.api.Tags;
 import cucumber.runtime.arquillian.domain.Belly;
