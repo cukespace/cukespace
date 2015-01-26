0f3ce289bcad925277cee0ef5dfb2b9a169d85b0
Jorge Gonzalez <jorgeejgonzalez@gmail.com>
2015-01-26 13:02:49 -0430

    fixed identation and spacing issues

--- core/src/main/java/cucumber/runtime/arquillian/feature/FeaturesFinder.java
+++ core/src/main/java/cucumber/runtime/arquillian/feature/FeaturesFinder.java
@@ -39,7 +39,6 @@ public final class FeaturesFinder {
 
     public static Map<String, Collection<URL>> createFeatureMap(final String tempDir, final String featureHome,
                                                                 final Class<?> javaClass, final ClassLoader classLoader) {
-    	
     	final Map<String, Collection<URL>> featuresMap = new HashMap<String, Collection<URL>>();
     	final String home = addSlashToHome(featureHome);
     	final Features additionalFeaturesAnnotations = javaClass.getAnnotation(Features.class);
@@ -49,62 +48,60 @@ public final class FeaturesFinder {
     		final FeatureSearchData featureData = new FeatureSearchData(home, tempDir, rawFeatureURL);
     		featuresMap.putAll(extractURLMapFromRawFeature(featureData, customResourceLoaders, classLoader, javaClass));
     	}
-        
-        featuresMap.putAll(extractURLMapFromResourceLoaders(customResourceLoaders, null, tempDir, javaClass));
-        
-        LOGGER.fine("Features: " + featuresMap);
-
-        return featuresMap;
+    	featuresMap.putAll(extractURLMapFromResourceLoaders(customResourceLoaders, null, tempDir, javaClass));
+    	
+    	LOGGER.fine("Features: " + featuresMap);
+    	return featuresMap;
     }
     
     private static String addSlashToHome(final String featureHome) {
     	if (featureHome != null && !featureHome.endsWith("/")) {
-            return featureHome + "/";
-        } else {
-            return featureHome;
-        }        
+    		return featureHome + "/";
+    	} else {
+    		return featureHome;
+    	}
     }
     
     private static Collection<ResourceLoader> retrieveCustomResourceLoaders(final Features features) {
     	final Collection<ResourceLoader> customLoaders = new LinkedList<ResourceLoader>();
-        customLoaders.addAll(CucumberLifecycle.resourceLoaders());
-        if (features != null) {
-            final Class<? extends ResourceLoader>[] userLoaders = features.loaders();
-            for (final Class<? extends ResourceLoader> resourceLoader : userLoaders) {
-                try {
-                    final ResourceLoader instance = resourceLoader.newInstance();
-                    customLoaders.add(instance);
-                } catch (final Exception e) {
-                    throw new IllegalArgumentException("can't create a " + resourceLoader.getName(), e);
-                }
-            }
-        }
-        return customLoaders;
+    	customLoaders.addAll(CucumberLifecycle.resourceLoaders());
+    	if (features != null) {
+    		final Class<? extends ResourceLoader>[] userLoaders = features.loaders();
+    		for (final Class<? extends ResourceLoader> resourceLoader : userLoaders) {
+    			try {
+    				final ResourceLoader instance = resourceLoader.newInstance();
+    				customLoaders.add(instance);
+    			} catch (final Exception e) {
+    				throw new IllegalArgumentException("can't create a " + resourceLoader.getName(), e);
+    			}
+    		}
+    	}
+    	return customLoaders;
     }
     
     private static Collection<String> findFeatures(final Class<?> javaClass) {
-        final Collection<String> featureUrls = new ArrayList<String>();
+    	final Collection<String> featureUrls = new ArrayList<String>();
         { // convention
-            final String featurePath = getfeaturePath(javaClass);
-            featureUrls.add(featurePath);
+        	final String featurePath = getfeaturePath(javaClass);
+        	featureUrls.add(featurePath);
         }
         { // our API
-            final Features additionalFeaturesAnnotation = javaClass.getAnnotation(Features.class);
-            if (additionalFeaturesAnnotation != null) {
-                Collections.addAll(featureUrls, additionalFeaturesAnnotation.value());
-            }
-        }        
+        	final Features additionalFeaturesAnnotation = javaClass.getAnnotation(Features.class);
+        	if (additionalFeaturesAnnotation != null) {
+        		Collections.addAll(featureUrls, additionalFeaturesAnnotation.value());
+        	}
+        }
         { // cucumber-junit API
-            final CucumberOptions cucumberOptionsAnnotation = javaClass.getAnnotation(CucumberOptions.class);
-            if (cucumberOptionsAnnotation != null && cucumberOptionsAnnotation.features() != null) {
-                Collections.addAll(featureUrls, cucumberOptionsAnnotation.features());
-            }
+        	final CucumberOptions cucumberOptionsAnnotation = javaClass.getAnnotation(CucumberOptions.class);
+        	if (cucumberOptionsAnnotation != null && cucumberOptionsAnnotation.features() != null) {
+        		Collections.addAll(featureUrls, cucumberOptionsAnnotation.features());
+        	}
         }
         return featureUrls;
     }
     
     private static String getfeaturePath(final Class<?> javaClass) {
-        return javaClass.getPackage().getName().replace('.', '/') + '/' + createClassNameSubPackage(javaClass.getSimpleName()) + EXTENSION;
+    	return javaClass.getPackage().getName().replace('.', '/') + '/' + createClassNameSubPackage(javaClass.getSimpleName()) + EXTENSION;
     }
     
     private static String createClassNameSubPackage(final String name) {
@@ -167,7 +164,7 @@ public final class FeaturesFinder {
     private static Map<String, Collection<URL>> extractUrlMapFromDirectResource(final FeatureSearchData featureData, final ClassLoader classLoader) throws IllegalStateException {
     	final Map<String, Collection<URL>> featureURLMap = new HashMap<String, Collection<URL>>();
     	
-        try {
+    	try {
     		featureURLMap.put(featureData.getRawFeatureURL() + featureData.getURLSuffix(), createUrlsListFromClassPath(classLoader, featureData.getURLPath()));
     		return featureURLMap;
     	} catch(NullPointerException npe) {
@@ -177,7 +174,7 @@ public final class FeaturesFinder {
     	try {
     		featureURLMap.put(featureData.getURLPath() + featureData.getURLSuffix(), createUrlsListFromFileSystem(featureData.getURLPath()));
     		return featureURLMap;    		
-    	} catch(NullPointerException npe) {
+    	} catch (NullPointerException npe) {
     		//do nothing
     	}
 
@@ -185,7 +182,7 @@ public final class FeaturesFinder {
     	try {
     		featureURLMap.put(featureData.getURLPath() + featureData.getURLSuffix(), createUrlsListFromFileSystem(featureData.getFeatureHome() + featureData.getURLPath()));
     		return featureURLMap;
-    	} catch(NullPointerException npe) {
+    	} catch (NullPointerException npe) {
     		//do nothing
     	}
     	throw new IllegalStateException();
@@ -230,19 +227,19 @@ public final class FeaturesFinder {
     	for (final ResourceLoader resourceLoader : resourceLoaders) {
     		try {
     			for (final Resource resource : resourceLoader.resources(resourcePath, EXTENSION)) {
-                    try {
-                        final File featureFileDump = getFeatureFileDumpFromResource(resource, tempDir, javaClass);
-                        featureURLMap.put(resource.getPath(), asList(featureFileDump.toURI().toURL()));
-                    } catch (final IOException e) {
-                        throw new IllegalStateException(e);
-                    }
-                }    		 
+    				try {
+    					final File featureFileDump = getFeatureFileDumpFromResource(resource, tempDir, javaClass);
+    					featureURLMap.put(resource.getPath(), asList(featureFileDump.toURI().toURL()));
+    				} catch (final IOException e) {
+    					throw new IllegalStateException(e);
+    				}
+                }
     		} catch (final NullPointerException npe) {
                 // no-op: we call it with null, don't expect miracles
             } catch (final IllegalArgumentException npe) {
                 // no-op: we call it with null, don't expect miracles
-            }            
-        }    	
+            }
+    	}
     	return featureURLMap;
     }
     
@@ -255,24 +252,22 @@ public final class FeaturesFinder {
     }
     
     private static Map<String, Collection<URL>> extractURLMapFromCucumberSearcher(final ClassLoader classLoader, final FeatureSearchData featureData) throws IllegalStateException {
-    	final Map<String, Collection<URL>> featureURLMap = new HashMap<String, Collection<URL>>();    	
-    	featureURLMap.put(featureData.getURLPath() + featureData.getURLSuffix(), createListFromCucumberSearcher(classLoader, featureData));    	
-        return featureURLMap;
+    	final Map<String, Collection<URL>> featureURLMap = new HashMap<String, Collection<URL>>();
+    	featureURLMap.put(featureData.getURLPath() + featureData.getURLSuffix(), createListFromCucumberSearcher(classLoader, featureData));
+    	return featureURLMap;
     }
     
     private static List<URL> createListFromCucumberSearcher(final ClassLoader classLoader, final FeatureSearchData featureData) throws IllegalStateException {
-    	final List<URL> urlsList = new ArrayList<URL>();        
-        try {
-        	urlsList.addAll(createUrlsListFromResource(classLoader, featureData.getURLPath()));
-        } catch(Exception e)
-        {
+    	final List<URL> urlsList = new ArrayList<URL>();
+    	try {
+    		urlsList.addAll(createUrlsListFromResource(classLoader, featureData.getURLPath()));
+    	} catch(Exception e) {
         	//do nothing
         }
         
         try {
         	urlsList.addAll(createUrlsListFromResource(classLoader, featureData.getFeatureHome()+featureData.getURLPath()));
-        } catch(Exception e)
-        {
+        } catch(Exception e) {
         	//do nothing
         }
         
@@ -300,19 +295,19 @@ public final class FeaturesFinder {
             	urlsList.add(extractedURL);
             } else {
             	LOGGER.warning("Resource " + resource + " ignored (unknown type).");
-            }            
+            }
         }
         
         if(urlsList.isEmpty()) {
         	throw new IllegalStateException();
-        }        
+        }
         return urlsList;
     }
     
     private static URL extractURLFromResource(final Resource resource, final ClassLoader classLoader) {    	
-    	if (FileResource.class.isInstance(resource)) {            
-            return extractURLFromFileResource(FileResource.class.cast(resource));
-        } else if (ZipResource.class.isInstance(resource)) {
+    	if (FileResource.class.isInstance(resource)) {
+    		return extractURLFromFileResource(FileResource.class.cast(resource));
+    	} else if (ZipResource.class.isInstance(resource)) {
             return extractURLFromZipResource(resource, classLoader);
         } else {
             return null;
@@ -343,7 +338,7 @@ public final class FeaturesFinder {
     	private String urlPath;
     	private String urlSuffix;
     	
-    	public FeatureSearchData(final String featureHome, final String tempDir, final String rawFeatureUrl) {    		
+    	public FeatureSearchData(final String featureHome, final String tempDir, final String rawFeatureUrl) {
     		this.featureHome = featureHome;
     		this.tempDir = tempDir;
     		this.rawFeatureURL = rawFeatureUrl;
@@ -360,20 +355,20 @@ public final class FeaturesFinder {
         private String extractFeatureURLPath(final int lineSeparatorIndex, final String rawFeatureURL) {
         	final String path;     	
         	if (lineSeparatorIndex > 0 && lineSeparatorIndex + 1 != MultiLoader.CLASSPATH_SCHEME.length()) {
-                 path = rawFeatureURL.substring(0, lineSeparatorIndex);             
-            } else {             
-                 path = rawFeatureURL;
-            }    	
+                 path = rawFeatureURL.substring(0, lineSeparatorIndex);
+        	} else {
+        		path = rawFeatureURL;
+            }
         	return path;
         }
         
         private String extractFeatureURLSuffix(final int lineSeparatorIndex, final String rawFeatureURL) {
-        	final String suffix;    	
+        	final String suffix;
         	if (lineSeparatorIndex > 0 && lineSeparatorIndex + 1 != MultiLoader.CLASSPATH_SCHEME.length()) {
                  suffix = rawFeatureURL.substring(lineSeparatorIndex);
             } else {
                  suffix = "";
-            }    	
+            }
         	return suffix;
         }
 
