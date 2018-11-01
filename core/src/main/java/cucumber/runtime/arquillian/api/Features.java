ab2410c08b67ba0997bba29903fa735fa3ebda00
toepi <toepi@users.noreply.github.com>
2018-11-01 09:39:07 +0100

    deprecated @Glues and @Features annoation
    Closes #39

--- core/src/main/java/cucumber/runtime/arquillian/api/Features.java
+++ core/src/main/java/cucumber/runtime/arquillian/api/Features.java
@@ -9,6 +9,11 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * 
+ * @deprecated use CucumberOptions instead.
+ */
+@Deprecated
 @Inherited
 @Target(TYPE)
 @Retention(RUNTIME)
